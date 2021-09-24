438ba95ea5d254df3ac00840f254d2e110795d56
WubzyGD <wubzygd@gmail.com>
2021-09-24 09:19:51 -0600

    fix avatar

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,14 +15,14 @@ export default {
             {name: 'theme-color', content: "#a172a6"},
             {
                 name: "og:image",
-                content: "https://cdn.discordapp.com/avatars/330547934951112705/a_a9d63b8c1f37d9096527ee24fbe9b587.gif?size=512"
+                content: "https://cdn.discordapp.com/avatars/330547934951112705/a_3f507224356810d225b8e1ada0270bc4.gif?size=512"
             }
         ],
         link: [
             {
                 rel: 'icon',
                 type: 'image/x-icon',
-                href: 'https://cdn.discordapp.com/avatars/330547934951112705/a_a9d63b8c1f37d9096527ee24fbe9b587.gif?size=128'
+                href: 'https://cdn.discordapp.com/avatars/330547934951112705/a_3f507224356810d225b8e1ada0270bc4.gif?size=128'
             },
             {rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap"},
             {rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap"}
